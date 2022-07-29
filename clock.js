7867f3a7b41e9068db4347034cfcc00be82f25bf
Berk Karataş <berk-karatas@protonmail.com>
2022-07-29 22:36:19 +0300

    dynamic clock with js

--- clock.js
+++ clock.js
@@ -3,3 +3,11 @@ function name(){
      var namedoc = document.getElementById("myName");
      namedoc.innerHTML = name;
 }
+
+function showTime(){
+     let date = new Date().toLocaleString('tr-TR'); 
+    document.getElementById('myClock').innerHTML=date;
+    setTimeout(showTime, 1000);  
+}
+
+setInterval(showTime, 1000);
